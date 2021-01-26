13af37c084893f7000875441f04d5344a3536f24
Jen Wood <jen.miriel@gmail.com>
2021-01-25 21:17:32 -0700

    setup app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateReservationComponent } from './pages/create-reservation/create-reservation.component';
+import { ViewInventoryComponent } from './pages/view-inventory/view-inventory.component';
+import { CreateInventoryComponent } from './pages/create-inventory/create-inventory.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CreateReservationComponent,
+  },
+  {
+    path: 'view-inventory',
+    component: ViewInventoryComponent,
+  },
+  {
+    path: 'create-inventory',
+    component: CreateInventoryComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateReservationComponent } from './pages/create-reservation/create-reservation.component';
+import { ViewInventoryComponent } from './pages/view-inventory/view-inventory.component';
+import { RouterModule } from '@angular/router';
+import { CreateInventoryComponent } from './pages/create-inventory/create-inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateReservationComponent,
+    ViewInventoryComponent,
+    CreateInventoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
